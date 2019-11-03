fdc2e16dd4ebf6d13be78ab389d41eeae7047dd8
Juan <juanpabloguleal@gmail.com>
2019-11-03 17:12:27 -0600

    The user profile is created during the registration

--- FinanceTracking/Controllers/HomeController.cs
+++ FinanceTracking/Controllers/HomeController.cs
@@ -5,20 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FinanceTracking.Models;
+using FinanceTracking.Data;
 
 namespace FinanceTracking.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
